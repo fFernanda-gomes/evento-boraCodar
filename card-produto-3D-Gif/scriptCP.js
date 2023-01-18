2fa6dd9e2a9b7adaef090afa331f2478777ab847
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-01-18 06:29:31 -0300

    html e CSS prontos antes do JS ser começado

---
+++ card-produto-3D-Gif/scriptCP.js
@@ -0,0 +1,6 @@
+// var buttonMove = document.getElementsByClassName('button-360')
+// var sofaImg = document.getElementsByClassName('sofaImg')
+
+// buttonMOve.addEventListener("click", () => {
+//     var sofaGif = 
+// })
