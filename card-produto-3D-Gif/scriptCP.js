b7ebf615c94ac3602ce3bb3447f6152a4ba60155
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-01-18 07:19:42 -0300

    projeto finalizado

--- card-produto-3D-Gif/scriptCP.js
+++ card-produto-3D-Gif/scriptCP.js
@@ -1,6 +1,42 @@
-// var buttonMove = document.getElementsByClassName('button-360')
-// var sofaImg = document.getElementsByClassName('sofaImg')
+const previewImg = document.getElementById('sofaImg')
+const productAnimation =document.getElementById('sofa-gif')
 
-// buttonMOve.addEventListener("click", () => {
-//     var sofaGif = 
-// })
+const openView = document.getElementById('buttonOpen')
+const closeView = document.getElementById('close-view')
+
+openView.addEventListener('click', () => {
+    openView.classList.add("hide");
+    closeView.classList.remove("hide");
+
+    previewImg.classList.add("hide");
+    productAnimation.classList.remove("hide");
+}) 
+
+closeView.addEventListener('click', () => {
+    openView.classList.remove("hide");
+    closeView.classList.add("hide");
+
+    previewImg.classList.remove("hide");
+    productAnimation.classList.add("hide");
+})
+
+/* forma fragmentada do código acima*/
+
+// function openAnimation () {
+//     openView.classList.add("hide")
+//     closeView.classList.remove("hide")
+
+//     previewImg.classList.add("hide")
+//     productAnimation.classList.remove("hide")
+// }
+
+// function closeAnimation () {
+//     openView.classList.remove("hide")
+//     closeView.classList.add("hide")
+
+//     previewImg.classList.remove("hide")
+//     productAnimation.classList.add("hide")
+// }
+
+// openView.addEventListener("click", openAnimation)
+// closeView.addEventListener("click", closeAnimation)
