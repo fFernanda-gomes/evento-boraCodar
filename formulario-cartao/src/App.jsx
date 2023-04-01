eb1ae090891d92cf21415a3a0a59d7da5b185817
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-04-01 17:38:22 -0300

    base from new project maked with React.js

---
+++ formulario-cartao/src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/card/Card"
+
+function App() {
+  return (
+    <div className="bg-form">
+      <div>{Card()}</div>
+    </div>
+  )
+}
+
+export default App
---
+++ formulario-cartao/src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.scss'
+
+function Card() {
+    return (
+        <>
+            <div className="cc-logo"></div>
+            <div className="cc-info"></div>
+        </>
+    )
+}
+
+export default Card
