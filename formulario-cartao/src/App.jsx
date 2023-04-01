4f4ae291be6a76ca459797a8837e1c2a51a1fcf5
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-04-01 20:53:08 -0300

    add image

--- formulario-cartao/src/App.jsx
+++ formulario-cartao/src/App.jsx
@@ -1,10 +1,15 @@
+import './App.scss'
 import Card from "./components/card/Card"
 
 function App() {
   return (
-    <div className="bg-form">
-      <div>{Card()}</div>
-    </div>
+    <main>
+      <div className="bg-form">
+        <div className="form">
+          <div>{Card()}</div>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- formulario-cartao/src/components/card/Card.jsx
+++ formulario-cartao/src/components/card/Card.jsx
@@ -1,11 +1,17 @@
 import './Card.scss'
+import bgCC from '/src/assets/bg-cc.png'
+import logoVisa from '/src/assets/logo-visa.svg'
+import iconAprox from '/src/assets/aprox.svg'
 
 function Card() {
     return (
-        <>
-            <div className="cc-logo"></div>
-            <div className="cc-info"></div>
-        </>
+       <div className="cc">
+        <div className="bg-cc">      
+        </div>
+        <div className="cc-logo">
+            <img src={logoVisa} alt="" />
+        </div>
+       </div>
     )
 }
 
