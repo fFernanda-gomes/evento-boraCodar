e1bf956d66c4f0f4f53d5ab03d26a795764c9d0d
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-04-14 16:29:36 -0300

    finish based style

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -7,42 +7,43 @@ import minus from './assets/minus.svg';
 import plus from './assets/plus.svg';
 import plusMinus from './assets/plusMinus.svg';
 import equals from './assets/equals.svg';
+import equalsTop from './assets/equals-top.svg'
 
 function App() {
   return (
       <div className="master">
         <div className="bgCal">
           <div className="top">
-
+            <img src={equalsTop} alt="" />
           </div>
           <div className="keyboard">
-            <button>CE</button>
+            <button className='first'>CE</button>
             <button>C</button>
             <button>
               <img src={percent} alt="" />
             </button>
-            <button>
+            <button className='bg-purple'>
             <img src={divide} alt="" />
             </button>
 
             <button>7</button>
             <button>8</button>
             <button>9</button>
-            <button>
+            <button className='bg-purple'>
             <img src={X} alt="" />
             </button>
 
             <button>4</button>
             <button>5</button>
             <button>6</button>
-            <button>
+            <button className='bg-purple'>
             <img src={minus} alt="" />
             </button>
 
             <button>1</button>
             <button>2</button>
             <button>3</button>
-            <button>
+            <button className='bg-purple'>
             <img src={plus} alt="" />
             </button>
 
@@ -51,7 +52,7 @@ function App() {
             </button>
             <button>0</button>
             <button>,</button>
-            <button>
+            <button className='bg-change'>
             <img src={equals} alt="" />
             </button>
           </div>
